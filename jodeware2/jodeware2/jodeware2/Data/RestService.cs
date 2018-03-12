06b4ca16d441fe1513ac8351cc7351473f7f7992
Janka <janka@me.com>
2018-03-12 19:55:05 +0100

    commit - added RestService.cs

---
+++ jodeware2/jodeware2/jodeware2/Data/RestService.cs
@@ -0,0 +1,32 @@
+﻿using jodeware2.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jodeware2.Data
+{
+    class RestService
+    {
+        HttpClient client;
+        string grant_type = "password";
+
+        public RestService()
+        {
+            client = new HttpClient();
+            client.MaxResponseContentBufferSize = 256000;
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded' "));
+        }
+
+        public async Task<Token> Login(User user)
+        {
+            var postData = new List<KeyValuePair<string, string>>();
+            postData.Add(new KeyValuePair<string, string>("grant_type", grant_type));
+            postData.Add(new KeyValuePair<string, string>("username", user.username));
+            postData.Add(new KeyValuePair<string, string>("password", user.password));
+            var content = new FormUrlEncodedContent(postData);
+            
+        }
+    }
+}
