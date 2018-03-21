f50a70ddd1171f35c8534c2ecd8ac6be9ca20ce1
Christopher Aknaj <jan15921@spengergasse.at>
2018-03-21 17:17:15 +0100

    commit - ListView für Bericht?

--- jodeware2/jodeware2/jodeware2/View/Bericht.xaml.cs
+++ jodeware2/jodeware2/jodeware2/View/Bericht.xaml.cs
@@ -9,12 +9,17 @@ using Xamarin.Forms.Xaml;
 
 namespace jodeware2.View
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Bericht : ContentPage
-	{
-		public Bericht ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Bericht : ContentPage
+    {
+        public Bericht()
+        {
+            InitializeComponent();
+        }
+
+        async void Button_Clicked(Object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new HomeScreen());
+        }
+    }
 }
