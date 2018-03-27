2d9d8701d9753b28b6036f5d5bbbcf92f22ba646
Muhammet Esen <ese14833@spengergasse>
2018-03-27 17:01:46 +0200

    An Kamera - Funktion gearbeitet.

--- jodeware2/jodeware2/jodeware2/View/Adden.xaml.cs
+++ jodeware2/jodeware2/jodeware2/View/Adden.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using jodeware2.Models;
+using Plugin.Media;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -58,5 +59,38 @@ namespace jodeware2.View
             imageSender.Aspect = Aspect.AspectFit;
             await Navigation.PushModalAsync(new Bericht());
         }
+
+        /*async void Btn_kamera(object sender, EventArgs e)
+        {
+            await CrossMedia.Current.Initialize();
+            if(!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("Keine Kamera gefunden", "Bitte aktivieren Sie Ihren Kamera!", "Okay");
+                return;
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(
+                new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                });
+
+            if(file == null)
+            {
+                return;
+            }
+
+            MainImage.S
+        }*/
+
+
+
+
+        /* Vorrübergehend */
+        async void Btn_kamera(object sender, EventArgs e)
+        {
+            await DisplayAlert("Foto", "Demnächst kommt die vollständige Funktionalität", "Okay");
+        }
+
     }
 }
